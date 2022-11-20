61207cba9ee39790f4f252285037b6f8bc42949f
ileana <ileanariv78@gmail.com>
2022-11-19 21:16:59 -0600

    cambios1

--- src/components/IndexAlumno.js
+++ src/components/IndexAlumno.js
@@ -12,7 +12,7 @@ export const IndexA = () => {
             <h4 class="titulo2"> <a href="/">Cerrar Sesion</a></h4>
           </div>
           <div class="menu">
-            <a href="/crear" class="d-block text-light p-3">Inicio del sitio</a><br/>
+            <a href="/alumnoHome" class="d-block text-light p-3">Inicio del sitio</a><br/>
           </div>
           <div class="menu">
             <a href="/showActividadesAlumnos" class="d-block text-light p-3">Actividades</a> <br/>
